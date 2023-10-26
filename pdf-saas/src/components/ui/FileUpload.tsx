d49fc0a12b5a5a4786d46ecefaac3c91dd0d7d5b
Ehab Okal <ihabokal@hotmail.com>
2023-10-26 23:57:12 +0200

    setup s3 upload

--- pdf-saas/src/components/ui/FileUpload.tsx
+++ pdf-saas/src/components/ui/FileUpload.tsx
@@ -1,15 +1,26 @@
 "use client";
+import { uploadToS3 } from "@/lib/s3";
 import { Inbox } from "lucide-react";
 import React from "react";
 import { useDropzone } from "react-dropzone";
 
 const FileUpload = () => {
   const { getRootProps, getInputProps } = useDropzone({
-    accept: {'application/pdf':[".pdf"]},
+    accept: { "application/pdf": [".pdf"] },
     maxFiles: 1,
-    onDrop:(acceptedFiles)=>{
-        console.log(acceptedFiles)
-    }
+    onDrop: async (acceptedFiles) => {
+      console.log(acceptedFiles);
+      const file = acceptedFiles[0];
+      if (file.size > 10 * 1024 * 1024) {
+        alert("please upload a smaller file than 10MB");
+        return;
+      }
+      try {
+        const data = await uploadToS3(file);
+      } catch (error) {
+        console.error(error);
+      }
+    },
   });
   return (
     <div className="p-2 bg-white rounded-xl">
